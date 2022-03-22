17be207089188437ec16ccb38fd7339ea516921b
Lucaskyy <contact@sculas.xyz>
2022-03-22 21:23:17 +0100

    chore: use prettier

--- validator/index.js
+++ validator/index.js
@@ -1,16 +1,16 @@
 const Ajv2019 = require("ajv/dist/2019");
 const ajv = new Ajv2019({
-    allErrors: true
+  allErrors: true,
 });
 const glob = require("glob");
 
 function error(err) {
-    console.error(err);
-    process.exit(1);
+  console.error(err);
+  process.exit(1);
 }
 
 const globOpts = {
-    absolute: true
+  absolute: true,
 };
 
 let schema = {};
@@ -34,11 +34,11 @@ var files = glob.sync("**/*.!(schema).json", globOpts);
 
 if (files.length == 0) error("No JSON files found");
 
-dataArray = files.map(e => {
-    return {
-        path: e,
-        json: require(e)
-    }
+dataArray = files.map((e) => {
+  return {
+    path: e,
+    json: require(e),
+  };
 });
 
 console.log(`Found ${dataArray.length} JSON files!`);
@@ -46,12 +46,12 @@ console.log(`Found ${dataArray.length} JSON files!`);
 const validate = ajv.compile(schema);
 let valid = true;
 
-dataArray.forEach(data => {
-    console.log(`Validating file: ${data.path}`);
-    
-    valid = validate(data.json);
-    if (!valid) console.error(validate.errors);
-})
+dataArray.forEach((data) => {
+  console.log(`Validating file: ${data.path}`);
+
+  valid = validate(data.json);
+  if (!valid) console.error(validate.errors);
+});
 
 if (!valid) error("Validation failed");
 else console.log("All valid!");
